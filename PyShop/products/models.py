2931a2b348be1d1a58fae30022654269f0f20e3f
rejRoky <rejaul.islam.roky@gmail.com>
2022-05-26 07:38:45 +0600

    Product Model migrated in Pyshop

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product (models.Model) :
+    pName = models.CharField(max_length = 255)
+    pPrice = models.FloatField()
+    pStock = models.IntegerField()
+    pImage_url = models.CharField(max_length = 2083)
+
+
+
+
+
+
