c3a481757f99bdbee9317926968edc71ea2d79aa
rejRoky <rejaul.islam.roky@gmail.com>
2022-05-26 08:07:03 +0600

    SuperUser added

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Product (models.Model) :
-    pName = models.CharField(max_length = 255)
+
+class Product (models.Model):
+    pName = models.CharField(max_length=255)
     pPrice = models.FloatField()
     pStock = models.IntegerField()
-    pImage_url = models.CharField(max_length = 2083)
+    pImage_url = models.CharField(max_length=2083)
 
 
-
-
-
-
+class Offer(models.Model):
+    pCode = models.CharField(max_length=10)
+    pDescription = models.CharField(max_length=255)
+    pDiscount = models.FloatField()
